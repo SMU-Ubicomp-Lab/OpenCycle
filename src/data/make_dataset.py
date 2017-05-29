fec2895bae767505b466102565e73b04ef2d5a14
aryamccarthy <arya.mccarthy@gmail.com>
2017-05-28 22:06:48 -0500

    Handle loading and processing data with Makefile

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,89 @@
 # -*- coding: utf-8 -*-
-import os
-import click
-import logging
-from dotenv import find_dotenv, load_dotenv
+# In[1]:
+
+from os import path
+
+import numpy as np
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+# In[2]:
+
+try:
+    file = path.join("data", "raw", "london.csv")
+except OSError:
+    print("This repository does not host the data. "
+          "Put the csv in ../data/raw/")
+    raise
+df = pd.read_csv(file, na_values=0)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+# Rename Italian columns to English.
 
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+# In[3]:
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+df.rename(columns={'DONNA': 'ID',
+                   'P_SPEZZ': 'SEGMENT_ID',
+                   'P_CICLO': 'CYCLE_ID',
+                   'ANNO_NAS': 'BIRTH_YR',
+                   'DATA': 'BEGIN_DATE',
+                   'T_SPEZZ': 'N_SEGMENTS',
+                   'T_CICLI': 'N_CYCLES',
+                   'QUALIFI': 'DESC',
+                   'TIPOTEMP': 'TEMP_SCALE',
+                   'L_CICLO': 'L_CYCLE',
+                   'L_PREOV': 'L_PREOVULATION',
+                   'L_PERIOD': 'L_PERIOD',
+                   'FIGLI': 'CHILDREN'
+                   },
+          inplace=True)
 
-    main()
+
+# We delete the cycles (rows) where the `L_PREOV` is missing (NA), because this data is unusable - we don't have the variable we seek to predict. **Ex:** If row 100 is deleted, the index would be $..., 98, 99, 101, 102, ...$
+#
+# After these rows are dropped, there are holes created in the index of the data frame. We would like the index numbers to be consecutive, so we use the `reset_index` function.
+
+# In[4]:
+
+df.dropna(subset=['L_PREOVULATION'], inplace=True)
+
+
+# Delete those where DESC != 1 because those are flawed entries.
+
+# In[5]:
+
+df = df[df.DESC == 1]
+df.drop('DESC', 1, inplace=True)
+
+
+# # Process Temperature Readings
+# 
+# Decode temperatures to obtain *real* BBT values. Then, convert all celsius values to fahrenheit. The coding is described in [this file](../references/BBTProcessing.txt).
+
+# In[6]:
+
+FAHR = 1
+CELS = 2
+for i in range (1,100):
+    df.loc[df.TEMP_SCALE == FAHR, 'TEMP' + str(i)] =  90 + df.loc[df.TEMP_SCALE == FAHR, 'TEMP' + str(i)]/10
+    df.loc[df.TEMP_SCALE == CELS, 'TEMP' + str(i)] =  30 + df.loc[df.TEMP_SCALE == CELS , 'TEMP' + str(i)]/10
+    #Convert celsius temps to fahrenheit
+    df.loc[df.TEMP_SCALE == CELS, 'TEMP' + str(i)] =  32 + ((9/5) *  df.loc[df.TEMP_SCALE == CELS , 'TEMP' + str(i)])
+
+#Display medians one at a time    
+#df[df.TEMP_SCALE==FAHR].median()
+#df[df.TEMP_SCALE==CELS].median()
+
+
+# Create an age measurement for each cycle, computed from the birth year and measurement date.
+
+# In[7]:
+
+#NEED TO FIX SO THAT WE HAVE ONE AGE PER ID NOT CYCLE
+df['AGE'] = (df.BEGIN_DATE.apply(lambda s: int(s.split('/')[-1]) % 100 if isinstance(s, str) else s) - df.BIRTH_YR)
+
+
+# In[8]:
+
+destination = ["data", "interim"]
+df.to_csv(path.join(*destination, "df.csv"))
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,8 +1,24 @@
 import numpy as np
-import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def _generic_residual_plot(x, y, title="", xaxis="", hue=None):
+    if hue is None:
+        hue = x
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, s=9, c=hue, cmap="winter")
+    ax.set_xlabel(xaxis)
+    ax.set_ylabel('difference from truth')
+    ax.set_title("Modified Bland-Altman Plot")
+
+    # Plot a horizontal line at 0
+    ax.axhline(0, ls=":", c=".2")
+
+    return ax
+
 
 def modified_bland_altman_plot(predicted, truth):
-    """Plot residuals of the given predictions.
+    """Plot residuals of the given predictions against truth.
 
     Parameters
     ----------
@@ -19,14 +35,37 @@ def modified_bland_altman_plot(predicted, truth):
 
     """
     predicted = np.asarray(predicted)
-    truth = np.asarray(truth, dtype=np.int)
-    diff = predicted - truth
+    truth = np.asarray(truth)
+    diff = truth - predicted  # stats.stackoverflow told me to.
 
-    ax = sns.stripplot(truth, diff, jitter=True)
-    ax.set(xlabel='truth', ylabel='difference from truth', title="Modified Bland-Altman Plot")
+    return _generic_residual_plot(truth, diff,
+                                  title="Modified Bland-Altman Plot",
+                                  xaxis="truth",
+                                  hue=predicted)
 
-    # Plot a horizontal line at 0
-    ax.axhline(0, ls=":", c=".2")
 
-    return ax
+def residual_plot(predicted, truth):
+    """Plot residuals of the given predictions against predicted (fitted) values.
 
+    Parameters
+    ----------
+    predicted : array-like, shape (n, )
+        The predicted values.
+
+    truth : array-like, shape (n, )
+        The true values.
+
+    Returns
+    -------
+    ax : matplotlib axis
+        The axis on which the plot was made.
+
+    """
+    predicted = np.asarray(predicted)
+    truth = np.asarray(truth)
+    diff = truth - predicted  # stats.stackoverflow told me to.
+
+    return _generic_residual_plot(predicted, diff,
+                                  title="Residuals",
+                                  xaxis="predicted",
+                                  hue=truth)
